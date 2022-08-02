587d984dc6422ffaa9ad1a4c7ad0f9a3ab099ff6
Ash4701 <wx13218076986@163.com>
2022-08-02 23:06:01 +0800

    cookie

--- init.py
+++ init.py
@@ -4,7 +4,7 @@ from ua_list import ua
 headers = {
     'User-Agent': ua.firefox,
     'Host': 'www.butian.net',
-            'Cookie': 'PHPSESSID=sih5h99khqeira5gpfhimt9bo3; notice=0; __btu__=ca4ac4bafe97cad9c69779f3eab6b301f88a63e0; __btc__=e60e43a09e08f4d6b5a8ed25477bcca12fff63bf; __btuc__=1339469e4fde826fd02b5268e0305cb8fbe21c4e',
+            'Cookie': '',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
             'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
             'Accept-Encoding': 'gzip, deflate',
